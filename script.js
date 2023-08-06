e114519ed33cb0d66796d163448d50406954aaa3
ddrana1 <ddrana1@myseneca.ca.com>
2023-08-05 22:30:14 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Your form submission logic here
+
+    // Display success alert
+    alert('Registered Successfully');
+});
