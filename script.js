bf0acd73b0080b2d4ca0a815a022f87251fffc3a
ddrana1 <ddrana1@myseneca.ca.com>
2023-08-10 11:00:40 -0400

    fourth commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*I, Divya Devendrasinh Rana (138704176) declare that my assessment is wholly my own work in accordance with Seneca Academic Policy.
+No part of this assessment has been copied manually or electronically from any other source 
+(including web sites)except for the information supplied by the WEB222 instructors and / or 
+made available in this assessment for my use. I also declare that no part of this assignment has been distributed to other students.
+*/
+
 // script.js
 document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
@@ -6,3 +12,5 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
     // Display success alert
     alert('Registered Successfully');
 });
+/*var submit = document.getElementById("submit");
+submit.addEventListener("click",calculateVolume, false);*/
